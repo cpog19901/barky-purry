64bdebd8b8260d1732a2f2d785cc1f6e23275ffb
Ciaran O'Grady <ciaranogrady8@gmail.com>
2021-06-24 00:57:24 +0100

    add test paragraph

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <h2>Barky Purry</h2>
     <button>Cats</button>
     <button>Dogs</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime distinctio corporis nisi ab deserunt vero tempora, voluptas ea consequuntur consequatur a quis id aut optio earum perspiciatis, ad exercitationem deleniti?</p>
     </div>
   );
 }
