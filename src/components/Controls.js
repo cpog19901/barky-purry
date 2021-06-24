85daf04b4ac5e777eae13ed5c130d3c4c45ebd40
Ciaran O'Grady <ciaranogrady8@gmail.com>
2021-06-24 10:27:14 +0100

    create FetchData component

---
+++ src/components/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Controls = () => {
+    return (
+        <div>
+    <button >Cats</button>
+    <button>Dogs</button>
+        </div>
+    );
+};
+
+export default Controls;
